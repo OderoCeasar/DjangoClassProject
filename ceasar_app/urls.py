8f6dafbdd0aa910c34f510c556383f0160173a37
Ceasar Odero <oderoceasar@gmail.com>
2024-11-07 23:50:51 +0300

    initial project setup

---
+++ ceasar_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from ceasar_app import views
+
+urlpatterns =[
+    path('', views.home, name="my_index"),
+    path('about/', views.about, name="my_about"),
+    path('contact/', views.contact, name="my_contact"),
+    path('blog/', views.blog, name="my_blog"),
+    path('services/', views.services, name="my_services"),
+
+]
